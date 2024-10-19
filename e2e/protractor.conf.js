19df730d2c2b0847e9722d92a37886614c124d3b
ASHISH SONAVANE <90111868+Ashishsonavane@users.noreply.github.com>
2024-10-20 04:16:07 +0530

    Update protractor.conf.js

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -26,3 +26,15 @@ exports.config = {
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
   }
 };
+ecfg_ab91ss33m5bisbsyzq5q8jsg1git
+import { NextResponse } from 'next/server';
+import { get } from '@vercel/edge-config';
+
+export const config = { matcher: '/welcome' };
+
+export async function middleware() {
+  const greeting = await get('greeting');
+  // NextResponse.json requires at least Next v13.1 or
+  // enabling experimental.allowMiddlewareResponseBody in next.config.js
+  return NextResponse.json(greeting);
+}
